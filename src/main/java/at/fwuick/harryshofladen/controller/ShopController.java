6314af8cfb114d703e20dc1669c9b03a735e146e
Lukas Schneider <wiiinator1@gmail.com>
2017-11-04 19:51:19 +0100

    + added Username to model for shopController always

--- src/main/java/at/fwuick/harryshofladen/controller/ShopController.java
+++ src/main/java/at/fwuick/harryshofladen/controller/ShopController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,6 +38,7 @@ public class ShopController {
 	
 
 	
+	
 	@Autowired
 	public ShopController(OrderableProductDao orderableProductDao, ShopProductConverter shopProductConverter,
 			IOrderService orderService, OrderDao orderDao, ListOrderConverter orderConverter) {
@@ -48,12 +50,16 @@ public class ShopController {
 		this.orderConverter = orderConverter;
 	}
 	
+	@ModelAttribute("username")
+	public String getUsername(){
+		return SecurityContextService.getUser().getName();
+	}
+	
 	@RequestMapping("/shop")
 	public String shop(Model model){
 		Collection<Product> products = orderableProductDao.all();
 		Collection<ShopProduct> shopProducts = products.stream().map(shopProductConverter::convert).collect(Collectors.toList());
 		model.addAttribute("products", shopProducts);
-		model.addAttribute("username", SecurityContextService.getUser().getName());
 		return "shop";
 	}
 	
